ca5099ac10cb45986fec5760396aef4c934504ab
Srivarun <nsrivarun@gmail.com>
2021-07-11 21:10:33 +0530

    Time: 1 ms (74.88%), Space: 38.8 MB (36.15%) - LeetHub

---
+++ one-edit-distance/one-edit-distance.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isOneEditDistance(String s, String t) {
+        if(s.length() > t.length())
+            return isOneEditDistance(t,s);
+        
+        if(s.equals(t)) return false;
+        
+        
+        for(int i = 0;i < s.length();i++)
+        {
+            if(s.charAt(i) != t.charAt(i))
+            {
+                if(s.length() == t.length())
+                    return s.substring(i+1).equals(t.substring(i+1));
+                else
+                    return s.substring(i).equals(t.substring(i+1));
+            }
+        }
+        return s.length()+1 == t.length();
+    }
+}
