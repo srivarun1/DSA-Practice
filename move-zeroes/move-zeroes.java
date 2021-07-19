e47dcde1db7e979496a1d43316103aed718d6599
Srivarun <nsrivarun@gmail.com>
2021-07-19 22:56:16 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (26.15%) - LeetHub

---
+++ move-zeroes/move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        for(int j = 0; j < nums.length; j++)
+        {
+            if(nums[j] != 0)
+            {
+                nums[i++] = nums[j];
+            }
+        }
+        
+        while(i<nums.length)
+        {
+            nums[i++] = 0;
+        }
+    }
+}
